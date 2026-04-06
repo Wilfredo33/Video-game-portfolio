380acd14a2259e0d2a79438452130073e98488de
Wilfredo Rivera <78219936+Wilfredo33@users.noreply.github.com>
2026-04-06 13:30:51 -0500

    Text modifications

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,7 +1,7 @@
 export default function CTA() {
     return (
         <section className="cta">
-            <a href="#projects" className="aaa-button">VER PORTAFOLIO</a>
+            <a href="#projects" className="aaa-button">VOLVER A PORTAFOLIO</a>
         </section>
     );
 }
